57f1ee4751b3aaac6f8804a342ce7526e0923882
Deniz <thetoasterofdoom1@gmail.com>
2019-03-06 09:55:49 -0500

    renamed to lower case

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char*[] argv) {
+	/*
+	 * Basic project functionality outline:
+	 *  - Get user input
+	 *     - Command line arguments
+	 *     - Standard input
+	 *     - Environment variable WORD_FREAK
+	 *  - Sanitize input
+	 *  - Iterate over input
+	 *  - Count each occurence of each word
+	 *     - Manage each occurence in a dictionary?
+	 */
+	switch(argc) {
+	// too few inputs
+	case 1:
+		exit(-1);
+	default:
+		// run
+	}
+	//Sanitize inputs
+
+	return 0;
+}

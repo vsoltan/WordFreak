dc5872f633819e1d009188f39dc5dc42a66eeaab
Valeriy Soltan <valeriy.soltan@gmail.com>
2019-03-15 17:42:33 -0400

    changes to hashmap, still not working as intended

--- hashmap.c
+++ hashmap.c
@@ -6,7 +6,6 @@
 
 // increase HASH_SIZE to reduce collision in the dictionary
 // should be a prime number
-#define HASH_SIZE 97
 
 // accepts pointer to a hashmap and initializes the array
 // returns the pointer to the hashmap passed
@@ -20,30 +19,14 @@ Hashmap *init_hashmap() {
 	return hm;
 }
 
-// implements a basic hash algorithm based on the value of the pointer passed
-// Uses RS Hash algorithm found in https://stackoverflow.com/questions/6136964/rs-hashing-program
-// static unsigned int get_hash(char *key) {
-// 	unsigned int a = 63689, b = 378551;
-// 	unsigned int hash = 0;
-//
-// 	for(char *c = key; *c != '\0'; ++c) {
-// 		hash = (hash * a) + (*c);
-// 		a *= b;
-// 	}
-// 	printf("Hash value: %u", hash % HASH_SIZE);
-// 	return hash % HASH_SIZE;
-// }
-
 // uses the hash algorithm djb2
 // implementation: http://www.cse.yorku.ca/~oz/hash.html
 unsigned long get_hash(char *key) {
 	unsigned long hash = 5381, hash_size = 97;
 	int c;
-
-	while (c = *key++) {
-		// printf("%c ", (char) c);
+	char *dummy = key;
+	while (c = *dummy++) {
 		hash = ((hash << 5) + hash) + c;
-		// printf("\nhash : %lu\n", hash % HASH_SIZE);
 	}
 	return hash % HASH_SIZE;
 }
@@ -55,22 +38,15 @@ HM_Entry *get_entry(Hashmap *hm, char *key) {
 
 	// printf("%p\n", hm->entries[get_hash(key)]);
 	for(HM_Entry *entry = hm->entries[get_hash(key)]; entry != NULL; entry = entry->next) {
-		printf("for loop iteration\n");
+		// printf("for loop iteration\n");
 		// because short circuit evaluation does not exist
 		if (entry != NULL) {
-			printf("entry is not null\n");
-			printf("key is : %s\n", key);
-			printf("entry is : %s\n", entry->key);
-			//if (strcmp(key, entry->key) == 0) {
-			// TODO: entry->key is not malloced?
 			if (strcmp(key , entry->key) == 0) {
-				printf("entered the if statement\n");
+				// printf("entered the if statement\n");
 				return entry;
 			}
 		}
-		printf("ended for loop\n");
 	}
-	// debugging
 	printf("entry does not exist [not an error]\n");
 	return NULL;
 }
@@ -78,41 +54,36 @@ HM_Entry *get_entry(Hashmap *hm, char *key) {
 // enters the key and value into the dictionary,
 // if the key doesn't exist, then create a new entry
 // if the key already exists, then replace it's current value with the passed value
-HM_Entry *set_entry(Hashmap *hm, char *key, int *value) {
-	HM_Entry *entry = get_entry(hm, key);
+HM_Entry *set_entry(Hashmap *hm, char *key) {
 
+	// NULL if empty
+	HM_Entry *entry = get_entry(hm, key);
+	if (entry == NULL) {
+		printf("there is nothing there before insertion\n");
+	}
 	int hash = get_hash(key);
+	printf("%i,\n", hash);
 	// if the entry not found
 	if (entry == NULL) {
 		// then create a new entry
 		entry = (HM_Entry *) malloc(sizeof(HM_Entry));
-		entry->key = key;
-
 		// could not malloc
-		if (entry == NULL || entry->key == NULL) {
+		if (entry == NULL) {
 			perror("malloc");
 			exit(EXIT_FAILURE);
 		}
 
-		(hm->entries)[hash] = entry;
+		entry->key = key;
+		// if entry is initialized for the first time, counter set to 1
+		entry->value = 1;
 
+		hm->entries[hash] = entry;
+		printf("insertion complete?\n");
+		printf("entries[hash] = %s\n", hm->entries[hash]->key);
 	} else {
 		// if entry already exists, then free the old value so it can be updated
-		free(entry->value);
+		entry->value = entry->value + 1;
 	}
-
-	// duplicates the value
-	// don't want the entry to have a pointer to what was passed to maintain encapsulation
-	int *value_copy = malloc(sizeof(int));
-	if (value_copy == NULL) {
-		perror("malloc");
-		exit(EXIT_FAILURE);
-	}
-
-	*value_copy = *value;
-
-	// assigns new/updated value
-	entry->value = value_copy;
 
 	return entry;
 }
--- hashmap.h
+++ hashmap.h
@@ -1,11 +1,11 @@
-
+#define HASH_SIZE 97
 // struct that models an individual entry in a hashmap
 // has values for the entry's name/key, and the value associated with that name/key
 // only works when the name is a "string" and the value is an int, as that is what's necessary for this project
 typedef struct hm_entry {
 	struct hm_entry *next;
 	char *key;
-	int *value;
+	int value;
 } HM_Entry;
 
 typedef struct hashmap {
@@ -22,4 +22,4 @@ unsigned long get_hash(char *c);
 HM_Entry *get_entry(Hashmap *hm, char *key);
 
 // enters the key and value into the dictionary, if the key already exists, then replace it's value with the passed value
-HM_Entry *set_entry(Hashmap *hm, char *key, int *value);
+HM_Entry *set_entry(Hashmap *hm, char *key);

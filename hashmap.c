b8c6adcca34319f12d404392a1153a287156463e
vsoltan <vsoltan@umass.edu>
2019-03-15 04:04:16 +0000

    got hashmap working on a fundamental level

--- hashmap.c
+++ hashmap.c
@@ -4,51 +4,74 @@
 #include <stdio.h>
 #include "hashmap.h"
 
-// increase HASH_SIZE to reduce collision in the dicttionary
+// increase HASH_SIZE to reduce collision in the dictionary
 // should be a prime number
 #define HASH_SIZE 97
 
 // accepts pointer to a hashmap and initializes the array
 // returns the pointer to the hashmap passed
-Hashmap *hashmap(Hashmap *hm) {
-	hm = malloc(sizeof(Hashmap));
-	
-	hm->entries = malloc(sizeof(HM_Entry) * HASH_SIZE);
+Hashmap *init_hashmap() {
+	Hashmap *hm = malloc(sizeof(Hashmap));
+	hm->entries = calloc(HASH_SIZE, sizeof(HM_Entry *));
 	if(hm == NULL || hm->entries == NULL) {
-		printf("could not malloc hashmap\n");
+		perror("malloc");
 		exit(EXIT_FAILURE);
 	}
-
 	return hm;
 }
 
-
 // implements a basic hash algorithm based on the value of the pointer passed
-// Uses RS Hash https://stackoverflow.com/questions/6136964/rs-hashing-program
-int get_hash(char *key) {
-	int b = 378551;
-	int a = 63689;
+// Uses RS Hash algorithm found in https://stackoverflow.com/questions/6136964/rs-hashing-program
+// static unsigned int get_hash(char *key) {
+// 	unsigned int a = 63689, b = 378551;
+// 	unsigned int hash = 0;
+//
+// 	for(char *c = key; *c != '\0'; ++c) {
+// 		hash = (hash * a) + (*c);
+// 		a *= b;
+// 	}
+// 	printf("Hash value: %u", hash % HASH_SIZE);
+// 	return hash % HASH_SIZE;
+// }
 
-	int hash = 0;
+// uses the hash algorithm djb2
+// implementation: http://www.cse.yorku.ca/~oz/hash.html
+unsigned long get_hash(char *key) {
+	unsigned long hash = 5381, hash_size = 97;
+	int c;
 
-	for(char *c = key; *c != '\0'; ++c) {
-		hash = (hash * a) + (*c);
-		a *= b;
+	while (c = *key++) {
+		// printf("%c ", (char) c);
+		hash = ((hash << 5) + hash) + c;
+		// printf("\nhash : %lu\n", hash % HASH_SIZE);
 	}
-
 	return hash % HASH_SIZE;
 }
 
 // gets the entry in the dictionary that corresponds to the passed value,
 // returns a pointer to the that entry
 HM_Entry *get_entry(Hashmap *hm, char *key) {
-	for(HM_Entry *entry = hm->entries[get_hash(key)]; entry != NULL; entry = entry->next) {
-		if(strcmp(key, entry->key) == 0) {
-			return entry;
-		}
-	}
+	// handles collision of keys in the hashmap
 
-	printf("entry does not exist\n");
+	// printf("%p\n", hm->entries[get_hash(key)]);
+	for(HM_Entry *entry = hm->entries[get_hash(key)]; entry != NULL; entry = entry->next) {
+		printf("for loop iteration\n");
+		// because short circuit evaluation does not exist
+		if (entry != NULL) {
+			printf("entry is not null\n");
+			printf("key is : %s\n", key);
+			printf("entry is : %s\n", entry->key);
+			//if (strcmp(key, entry->key) == 0) {
+			// TODO: entry->key is not malloced?
+			if (strcmp(key , entry->key) == 0) {
+				printf("entered the if statement\n");
+				return entry;
+			}
+		}
+		printf("ended for loop\n");
+	}
+	// debugging
+	printf("entry does not exist [not an error]\n");
 	return NULL;
 }
 
@@ -57,16 +80,17 @@ HM_Entry *get_entry(Hashmap *hm, char *key) {
 // if the key already exists, then replace it's current value with the passed value
 HM_Entry *set_entry(Hashmap *hm, char *key, int *value) {
 	HM_Entry *entry = get_entry(hm, key);
-	int hash = get_hash(key);
 
+	int hash = get_hash(key);
 	// if the entry not found
-	if(entry == NULL) {
+	if (entry == NULL) {
 		// then create a new entry
-		entry = malloc(sizeof(HM_Entry));
+		entry = (HM_Entry *) malloc(sizeof(HM_Entry));
+		entry->key = key;
 
 		// could not malloc
-		if(entry == NULL || entry->next == NULL || entry->key == NULL || entry->value == NULL) {
-			printf("could not sucessfully malloc\n");
+		if (entry == NULL || entry->key == NULL) {
+			perror("malloc");
 			exit(EXIT_FAILURE);
 		}
 
@@ -77,11 +101,11 @@ HM_Entry *set_entry(Hashmap *hm, char *key, int *value) {
 		free(entry->value);
 	}
 
-	// duplicates the value 
+	// duplicates the value
 	// don't want the entry to have a pointer to what was passed to maintain encapsulation
 	int *value_copy = malloc(sizeof(int));
-	if(value_copy == NULL) {
-		printf("could not malloc value_copy\n");
+	if (value_copy == NULL) {
+		perror("malloc");
 		exit(EXIT_FAILURE);
 	}
 
--- hashmap.h
+++ hashmap.h
@@ -13,13 +13,13 @@ typedef struct hashmap {
 } Hashmap;
 
 // "constructor" for hashmap struct
-struct hashmap *hashmap(struct hashmap *hm);
+Hashmap *init_hashmap();
 
 // calculates the hash value of a provided string using RS Hash
-int get_hash(char *c);
+unsigned long get_hash(char *c);
 
 // gets the entry in the dictionary that corresponds to the passed value, returns a pointer to the entry
-struct hm_entry *get_entry(struct hashmap *hm, char *key);
+HM_Entry *get_entry(Hashmap *hm, char *key);
 
 // enters the key and value into the dictionary, if the key already exists, then replace it's value with the passed value
-struct hm_entry *set_entry(struct hashmap *hm, char *key, int *value);
+HM_Entry *set_entry(Hashmap *hm, char *key, int *value);
